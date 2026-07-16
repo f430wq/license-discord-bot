4243b6eb60a306f56083a6e9061452d778fb5a11
f430wq <renzyreally@gmail.com>
2026-07-16 22:29:26 +0200

    Update and rename .gitkeep to panel.js

---
+++ src/commands/panel.js
@@ -0,0 +1,113 @@
+const {
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} = require("discord.js");
+
+const config = require("../config");
+
+
+module.exports = {
+
+    name: "panel",
+
+
+    async execute(interaction) {
+
+
+        const embed = new EmbedBuilder()
+
+            .setTitle(
+                config.panel.title
+            )
+
+            .setDescription(
+                config.panel.description
+            )
+
+            .setColor(
+                config.panel.color
+            )
+
+            .setFooter({
+                text:"License System"
+            });
+
+
+
+        const buttons =
+            new ActionRowBuilder()
+            .addComponents(
+
+                new ButtonBuilder()
+                    .setCustomId(
+                        config.buttons.redeem
+                    )
+                    .setLabel(
+                        "Redeem Key"
+                    )
+                    .setEmoji("🔑")
+                    .setStyle(
+                        ButtonStyle.Primary
+                    ),
+
+
+                new ButtonBuilder()
+                    .setCustomId(
+                        config.buttons.script
+                    )
+                    .setLabel(
+                        "Get Script"
+                    )
+                    .setEmoji("📜")
+                    .setStyle(
+                        ButtonStyle.Success
+                    ),
+
+
+                new ButtonBuilder()
+                    .setCustomId(
+                        config.buttons.role
+                    )
+                    .setLabel(
+                        "Get Role"
+                    )
+                    .setEmoji("🎖️")
+                    .setStyle(
+                        ButtonStyle.Secondary
+                    ),
+
+
+                new ButtonBuilder()
+                    .setCustomId(
+                        config.buttons.reset
+                    )
+                    .setLabel(
+                        "Reset HWID"
+                    )
+                    .setEmoji("🔄")
+                    .setStyle(
+                        ButtonStyle.Danger
+                    )
+
+            );
+
+
+
+        await interaction.reply({
+
+            embeds:[
+                embed
+            ],
+
+            components:[
+                buttons
+            ]
+
+        });
+
+
+    }
+
+};
