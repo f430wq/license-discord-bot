947273b312ed5a79711156f82a73fb7bd6880ef9
f430wq <renzyreally@gmail.com>
2026-07-16 22:37:25 +0200

    Update ready.js

--- src/events /ready.js
+++ src/events /ready.js
@@ -1,19 +1,9 @@
-const {
-    REST,
-    Routes,
-    SlashCommandBuilder
-} = require("discord.js");
-
-const fs = require("fs");
-const path = require("path");
-
-
 module.exports = {
 
-    name: "ready",
+    name:"ready",
 
 
-    async execute(client) {
+    execute(client){
 
 
         console.log(
@@ -21,111 +11,9 @@ module.exports = {
         );
 
 
-
-        const commands = [];
-
-        const commandsPath = path.join(
-            __dirname,
-            "../commands"
+        console.log(
+            `📡 Servers: ${client.guilds.cache.size}`
         );
-
-
-
-        const commandFiles = fs.readdirSync(commandsPath)
-            .filter(file => file.endsWith(".js"));
-
-
-
-        for(const file of commandFiles){
-
-
-            const command = require(
-                `../commands/${file}`
-            );
-
-
-
-            if(command.data){
-
-                commands.push(
-                    command.data.toJSON()
-                );
-
-            }
-
-            else {
-
-
-                commands.push(
-
-                    new SlashCommandBuilder()
-
-                    .setName(
-                        command.name
-                    )
-
-                    .setDescription(
-                        `Execute ${command.name}`
-                    )
-
-                    .toJSON()
-
-                );
-
-            }
-
-
-        }
-
-
-
-
-
-        const rest = new REST({
-
-            version:"10"
-
-        }).setToken(
-            process.env.DISCORD_TOKEN
-        );
-
-
-
-
-        try {
-
-
-            await rest.put(
-
-                Routes.applicationGuildCommands(
-
-                    process.env.CLIENT_ID,
-
-                    process.env.GUILD_ID
-
-                ),
-
-
-                {
-                    body:commands
-                }
-
-            );
-
-
-
-            console.log(
-                "✅ Slash commands registered"
-            );
-
-
-        } catch(error){
-
-            console.error(
-                error
-            );
-
-        }
 
 
     }
